ae8aef7ded739034a5ffec509459a6bdf67d9090
HayaoMaeda <sijvision1010@gmail.com>
2017-01-22 05:47:00 +0000

    article-first

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only:[:show, :edit, :update, :destroy]
+  def new
+  	@article = Article.new
+  end
+
+  def create
+  	@article = Article.new(params.require(:article).permit(:title, :body))
+  	@article.save
+  	redirect_to @article
+  end
+
+  def index
+  	@articles = Article.all
+  end
+
+  def show
+  	@article = Article.find(params[:id])
+  end
+
+  def edit
+  	@article =Article.find(params[:id])
+  end
+
+  def update
+  	@article.update(article_params) 
+  	redirect_to article_path(@article.id)
+  end
+
+  def destroy
+  	@article = Article.find(params[:id])
+  	@article.destroy
+  	redirect_to articles_path
+  end
+
+  private
+  	def set_article
+  		@article = Article.find(params[:id])
+  	end
+
+  	def article_params
+  		params.require(:article).permit(:title, :body)
+  	end
+end
